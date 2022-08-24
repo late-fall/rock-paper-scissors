7316032b7c91723324249e8fd22ba25ba17f8fea
late-fall <shawnsolee@gmail.com>
2022-08-24 01:08:22 -0400

    final project

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+
+const display = document.querySelector('h2');
+const ps = document.querySelector('#pscore');
+const cs = document.querySelector('#cscore');
+const msg = document.querySelector('#msg');
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0){
@@ -12,59 +19,96 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection.toLowerCase() === computerSelection){
-        console.log("tie!");
-        return 0;
+    if (playerSelection === computerSelection){
+        display.innerHTML = '<span style="color: #000">Tied</span>';
     }
-    else if (playerSelection.toLowerCase() === "rock"){
+    else if (playerSelection === "rock"){
         if (computerSelection == "paper"){
-            console.log("You lost!");
-            return -1;
+            display.innerHTML = '<span style="color: red">You lost..</span>';
+            cs.innerHTML++;
+            game(ps.innerText, cs.innerText);
         }
         else {
-            console.log("You win!");
-            return 1;
+            display.innerHTML = '<span style="color: blue">You Win!</span>';
+            ps.innerHTML++;
+            game(ps.innerText, cs.innerText);
         }
     }
-    else if (playerSelection.toLowerCase() === "scissors"){
+    else if (playerSelection === "scissors"){
         if (computerSelection == "paper"){
-            console.log("You win!");
-            return 1;
+            display.innerHTML = '<span style="color: blue">You Win!</span>';
+            ps.innerHTML++;
+            game(ps.innerText, cs.innerText);
         }
         else {
-            console.log("You lost!");
-            return -1;
+            display.innerHTML = '<span style="color: red">You lost..</span>';
+            cs.innerHTML++;
+            game(ps.innerText, cs.innerText);
         }
     }
     else {
         if (computerSelection == "rock"){
-            console.log("You win!");
-            return 1;
+            display.innerHTML = '<span style="color: blue">You Win!</span>';
+            ps.innerHTML++;
+            game(ps.innerText, cs.innerText);
+            
         }
         else {
-            console.log("You lost!");
-            return -1;
+            display.innerHTML = '<span style="color: red">You lost!</span>';
+            cs.innerHTML++;
+            game(ps.innerText, cs.innerText);
         }
     }
 }
 
-//game function plays 5 rounds and keep score and report winner
-function game(){
-    let score = 0;
-    for (let i = 0; i < 5; i++){
-        let playerSelection = prompt("What will you play?(rock/paper/scissors)");
-        score += playRound(playerSelection, getComputerChoice());
+function game(player, computer){
+    if (player == 5){
+        msg.textContent = "You won the game!";
+        openPopup();
     }
-
-    if (score === 0){
-        return "You tied the game";
-    }
-    else if (score > 0){
-        return "You won the game";
-    }
-    else {
-        return "You lost the game";
+    else if (computer == 5) {
+        msg.textContent = "You lost..";
+        openPopup();
     }
 }
 
-console.log(game());
+const btnRock = document.querySelector(".rock");
+btnRock.addEventListener('click', () => {
+  playRound("rock", getComputerChoice());
+});
+
+const btnPaper = document.querySelector(".paper");
+btnPaper.addEventListener('click', () => {
+  playRound("paper", getComputerChoice());
+});
+
+const btnScissors = document.querySelector(".scissors");
+btnScissors.addEventListener('click', () => {
+  playRound("scissors", getComputerChoice());
+});
+
+const popup = document.getElementById("popup");
+const popContainer = document.getElementById("pop-container");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+    popContainer.classList.add("open-pop-container");
+}
+
+const reset = document.getElementById("reset");
+reset.addEventListener('click', () => {
+    popup.classList.remove("open-popup");
+    popContainer.classList.remove("open-pop-container");
+    display.innerText = "Score 5 times to win!";
+    ps.innerHTML = 0;
+    cs.innerHTML = 0;
+});
+
+const img = Array.from(document.querySelectorAll('img'));
+img.forEach(img => img.addEventListener('click', () => {
+    img.classList.add("onclick");
+}));
+img.forEach(img => img.addEventListener('transitionend', () => {
+    img.classList.remove("onclick");
+}));
+
